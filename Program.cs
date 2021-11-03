ce1870baad33e5d265c7b6bc5e22759c76db671a
Wallace Tácio <wallacett.dev@gmail.com>
2021-11-03 11:50:38 -0300

    Adiciona resolução do exercício Peso Ideal ETEC 2021.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace PesoIdeal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double height, pesoIdeal;
+            string sex;
+
+            Console.Write("Digite sua altura em m........: ");
+            
+            height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Sexo [M]asculino / [F]eminino.: ");
+            
+            sex = Console.ReadLine().ToUpper().Trim().Substring(0,1);
+            Console.WriteLine();
+
+            if(sex == "M")
+            {
+                pesoIdeal = height * 72.7 - 58.0;
+
+                Console.Write("Seu peso ideal é: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write($"{pesoIdeal:N2}");
+                Console.ResetColor();
+                Console.WriteLine("Kg.");
+            }
+            else if(sex == "F")
+            {
+                pesoIdeal = height * 62.1 - 44.7;
+
+                Console.Write("Seu peso ideal é: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write($"{pesoIdeal:N2}");
+                Console.ResetColor();
+                Console.WriteLine("Kg.");
+            }
+            else
+            {
+                Console.WriteLine("Digite \"F\" para feminnino ou \"M\" para masculino");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# PesoIdeal
-Exercício Peso Ideal ETEC 2021
+## Exercício `PesoIdeal` ETEC 2021
+
+Solicite que o usuário digite sua altura e o seu sexo ('M' para masculino, 'F' para feminino). Calcule e exiba seu peso ideal.
+
+- Para homens, altura x 72.7 - 58.0
+- Para mulheres, altura x 62.1 - 44.7
+
+Ex.:
+
+```
+Digite sua altura em m..........: 1,73
+Sexo [M]asculino / [F]eminino...: M
+Seu peso ideal é 67,7kg.
+```
+ou
+```
+Digite sua altura em m..........: 1,73
+Sexo [M]asculino / [F]eminino...: F
+Seu peso ideal é 62,7kg.
+```
